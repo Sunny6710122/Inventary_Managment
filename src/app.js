7966e8a53f9f15f310af84218218180c75923ab8
Sunny6710122 <sk6710122@gmail.com>
2024-08-04 15:41:27 +0500

    Fourth Commit

--- src/app.js
+++ src/app.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const hbs = require("hbs");
 const path = require("path");
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
-port = 8000;
+const port = 8000;
 
 const admin = require('firebase-admin');
-const serviceAccount = require('../firebase.json'); // Replace with the path to your JSON file
 
 // Initialize Firebase Admin SDK
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
+    credential: admin.credential.cert({
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
+    })
 });
 
 const db = admin.firestore();
